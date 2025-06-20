8dfdc8fb1820ef0b815d936a14dd76df8b663c9f
Pirmannn012 <firmansyahassgf012@gmail.com>
2025-06-20 18:00:56 +0800

    Add files via upload

---
+++ UAS_firmansyah/keyValue.js
@@ -0,0 +1,7 @@
+db.kvCollection.insertMany([
+  { key: "username1", value: "john_doe" },
+  { key: "username2", value: "jane_smith" },
+  { key: "username3", value: "alex_king" },
+  { key: "username4", value: "sarah_moore" },
+  { key: "username5", value: "daniel_ray" }
+])
---
+++ UAS_firmansyah/wideColums.js
@@ -0,0 +1,7 @@
+db.customers.insertMany([
+  { id: 1, name: "Andi", age: 25, city: "Makassar", email: "andi@example.com" },
+  { id: 2, name: "Budi", age: 30, city: "Jakarta", phone: "081234567890" },
+  { id: 3, name: "Citra", age: 28, address: "Jl. Merdeka No.10", loyalty_points: 120 },
+  { id: 4, name: "Dewi", age: 35, city: "Surabaya", last_login: "2025-06-15" },
+  { id: 5, name: "Eka", age: 22, city: "Bandung", preferred_payment: "Gopay" }
+])
